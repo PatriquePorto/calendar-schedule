8bfa569b4a23ce6183d504e50ff979d751a47608
Patrique Porto <patrikrock@hotmail.com>
2023-05-03 10:44:04 -0300

    Implementing time_zone

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule Calendlex.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+      {:timex, "~> 3.7"},
       {:rename_project, "~> 0.1.0", only: :dev}
     ]
   end
